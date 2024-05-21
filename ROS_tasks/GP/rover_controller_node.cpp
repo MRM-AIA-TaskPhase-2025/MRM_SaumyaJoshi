af5aac04629b1b9100c1d1c0d3be1a0c7cc45617
saumyaaa-06 <saumyajoshi06@gmail.com>
2024-05-21 22:59:22 +0530

    1

--- ROS_tasks/GP/rover_controller_node.cpp
+++ ROS_tasks/GP/rover_controller_node.cpp
@@ -25,6 +25,7 @@ class Rover {
 public:
     double currentLatitude; // Current latitude
     double currentLongitude; // Current longitude
+    double currentYaw;
 
     // Constructor to initialize the rover's position
     Rover() {
@@ -48,48 +49,34 @@ public:
         currentLongitude = msg->longitude;
     }
 
+   
     // Callback function for receiving IMU data
-    // Callback function for receiving IMU data
-// Callback function for receiving IMU data
-// Callback function for receiving IMU data
-void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
-    // Convert orientation quaternion to Euler angles
-    tf::Quaternion orientation;
-    tf::quaternionMsgToTF(msg->orientation, orientation);
-    double roll, pitch, yaw;
-    tf::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
+    void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
+        // Convert orientation quaternion to Euler angles
+        tf::Quaternion orientation;
+        tf::quaternionMsgToTF(msg->orientation, orientation);
+        double roll, pitch, yaw;
+        tf::Matrix3x3(orientation).getRPY(roll, pitch, yaw);
 
-    // Update rover's current yaw value based on received IMU data
-    currentYaw = toDegrees(yaw);
-    std::cout << "Yaw (degrees): " << currentYaw << std::endl;
-}
+        // Update rover's current yaw value based on received IMU data
+        currentYaw = toDegrees(yaw);
+        std::cout << "Yaw (degrees): " << currentYaw << std::endl;
+    }
 
 
 
     // Method to calculate the bearing (angle) between two GPS coordinates
-   double calculateBearing(double lat1, double lon1, double lat2, double lon2) {
+    double calculateBearing(double lat1, double lon1, double lat2, double lon2) {
     double dLon = toRadians(lon2 - lon1);
     double y = sin(dLon) * cos(toRadians(lat2));
     double x = cos(toRadians(lat1)) * sin(toRadians(lat2)) -
                sin(toRadians(lat1)) * cos(toRadians(lat2)) * cos(dLon);
-    double bearing = atan2(y, x);
+    double bearing = atan2(x, y);
     return toDegrees(bearing);
 }
 
-
-    double getCurrentYaw() {
-        return currentYaw;
-    }
-
-private:
-    ros::NodeHandle nh;
-    ros::Subscriber gpsFixSub;
-    ros::Subscriber imuSub;
-    double currentYaw; // Current yaw value
-};
-
-// Calculate distance between two GPS coordinates using Haversine formula
-double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
+    // Calculate distance between two GPS coordinates using Haversine formula
+    double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
     double dLat = toRadians(lat2 - lat1);
     double dLon = toRadians(lon2 - lon1);
     double a = sin(dLat / 2) * sin(dLat / 2) +
@@ -98,6 +85,15 @@ double calculateDistance(double lat1, double lon1, double lat2, double lon2) {
     double c = 2 * atan2(sqrt(a), sqrt(1 - a));
     return EARTH_RADIUS * c;
 }
+
+
+private:
+    ros::NodeHandle nh;
+    ros::Subscriber gpsFixSub;
+    ros::Subscriber imuSub;
+    
+};
+
 
 int main(int argc, char **argv) {
     // Initialize ROS node
@@ -109,235 +105,95 @@ int main(int argc, char **argv) {
 
     // Wait for initial GPS fix to set the rover's initial position
     ros::Rate loop_rate(10); // 10 Hz
-    while (ros::ok() && (rover.currentLatitude == 0.0 || rover.currentLongitude == 0.0)) {
-        ros::spinOnce();
-        loop_rate.sleep();
-    }
 
     // Publish Twist messages to move the rover
     ros::Publisher cmdVelPub = nh.advertise<geometry_msgs::Twist>("/cmd_vel",20);
-
+    
     // Input target GPS coordinates
     double targetLatitude, targetLongitude;
     std::cout << "Enter target latitude: ";
     std::cin >> targetLatitude;
     std::cout << "Enter target longitude: ";
     std::cin >> targetLongitude; // Replace with your target longitude
+    ros::spinOnce();
 
-    // Calculate initial bearing from rover's current position to target
+    
     double initialBearing = rover.calculateBearing(rover.currentLatitude, rover.currentLongitude, targetLatitude, targetLongitude);
     std::cout << "Initial bearing: " << initialBearing << " degrees" << std::endl;
 
-    // Loop to continuously publish Twist messages
-  /*  while (ros::ok()) {
-        // Get the current location of the rover
-        double currentLatitude = rover.currentLatitude;
-        double currentLongitude = rover.currentLongitude;
 
-        // Calculate distance to target
-        double distance = calculateDistance(targetLatitude, targetLongitude, currentLatitude, currentLongitude);
-        std::cout << "Distance to target: " << distance << " meters" << std::endl;
-        std::cout << "Current latitude: " << currentLatitude << std::endl;
-        std::cout << "Current longitude: " << currentLongitude << std::endl;
-
-        // Check if rover has reached target position
-        if (distance < 0.7) {
-            std::cout << "Rover reached target position." << std::endl;
-            geometry_msgs::Twist twistMsg;
-            twistMsg.linear.x = 0.0;
-            twistMsg.angular.z = 0.0;
-            cmdVelPub.publish(twistMsg);
-            break; // Exit the loop
-        }
-
-        // Calculate current yaw
-        double currentYaw = rover.getCurrentYaw();
-        std::cout << "yaw:"  <<  currentYaw << std::endl;
-
-        // Calculate angle error between initial bearing and current yaw
-        double angleError = initialBearing - currentYaw;
-        if (angleError > 180.0) {
-            angleError -= 360.0;
-        } else if (angleError < -180.0) {
-            angleError += 360.0;
-        }
-         //angleError=std::abs(angleError);
-    std::cout << "error: " << angleError << std::endl;
-        // Adjust rover's orientation towards target
-        geometry_msgs::Twist twistMsg;
-        twistMsg.linear.x = 0.3; // Linear velocity (m/s)
-
-        // Check if yaw is equal to initial bearing, if yes, set angular velocity to 0
-        if (angleError<=0.1 && angleError>=-0.1) {
-            twistMsg.angular.z = 0.0;
-            break;
-        } else {
-            twistMsg.angular.z = angleError * 0.2; // Angular velocity (rad/s)
-            
-            
-        }
-         
-        std::cout << "av:"  <<  twistMsg << std::endl;
-        cmdVelPub.publish(twistMsg);
-
-        ros::spinOnce();
-        loop_rate.sleep();
-    } */
-/*while (ros::ok()) {
-// Get the current location of the rover
-        double currentLatitude = rover.currentLatitude;
-        double currentLongitude = rover.currentLongitude;
-
-        // Calculate distance to target
-        double distance = calculateDistance(targetLatitude, targetLongitude, currentLatitude, currentLongitude);
-        std::cout << "Distance to target: " << distance << " meters" << std::endl;
-        std::cout << "Current latitude: " << currentLatitude << std::endl;
-        std::cout << "Current longitude: " << currentLongitude << std::endl;
-
-        // Check if rover has reached target position
-        if (distance < 0.7) {
-            std::cout << "Rover reached target position." << std::endl;
-            geometry_msgs::Twist twistMsg;
-            twistMsg.linear.x = 0.0;
-            twistMsg.angular.z = 0.0;
-            cmdVelPub.publish(twistMsg);
-            break; // Exit the loop
-        }
-
-        geometry_msgs::Twist twistMsg;
-        twistMsg.linear.x = 0.3; // Linear velocity (m/s)
-        twistMsg.angular.z = 0.0;
-
-        std::cout << "av:"  <<  twistMsg << std::endl;
-        cmdVelPub.publish(twistMsg);
-
-        ros::spinOnce();
-        loop_rate.sleep();
-} */
-
-// Adjust rover's orientation towards initial bearing
-while (ros::ok()) {
-    // Get the current location of the rover
-    double currentLatitude = rover.currentLatitude;
-    double currentLongitude = rover.currentLongitude;
-
-    // Calculate angle error between initial bearing and current yaw
-    double currentYaw = rover.getCurrentYaw();
-    double angleError = initialBearing - currentYaw;
-    if (angleError > 180.0) {
-        angleError -= 360.0;
-    } else if (angleError < -180.0) {
-        angleError += 360.0;
-    }
-    std::cout << "Yaw: " << currentYaw << " Angle error: " << angleError << std::endl;
-
-    // Adjust rover's orientation towards initial bearing
-    geometry_msgs::Twist twistMsg;
-    twistMsg.linear.x = 0.0; // No linear movement during orientation adjustment
-    
-    if (angleError<=0.1 && angleError>=-0.1) {
-            twistMsg.angular.z = 0.0;
-            std::cout << twistMsg << std::endl;
-            if (ros::ok()) {
-            std::cout << "works1" << std::endl;
-            cmdVelPub.publish(twistMsg);
-            std::cout << "works2" << std::endl;
-            break;
-            } 
-    }
-            else {
-            twistMsg.angular.z = angleError * 0.2; // Angular velocity (rad/s)
-        }
-             
-     cmdVelPub.publish(twistMsg);
-
-
-
-    
-
-    ros::spinOnce();
-    loop_rate.sleep();
-}
-/*
-// Move rover linearly towards the target position while adjusting yaw
-bool isYawAligned = false; // Flag to track if yaw is aligned with initial bearing
 
 while (ros::ok()) {
+
     // Get the current location of the rover
     double currentLatitude = rover.currentLatitude;
     double currentLongitude = rover.currentLongitude;
 
     // Calculate distance to target
-    double distance = calculateDistance(targetLatitude, targetLongitude, currentLatitude, currentLongitude);
+    double distance = rover.calculateDistance(targetLatitude, targetLongitude, currentLatitude, currentLongitude);
     std::cout << "Distance to target: " << distance << " meters" << std::endl;
     std::cout << "Current latitude: " << currentLatitude << std::endl;
     std::cout << "Current longitude: " << currentLongitude << std::endl;
 
     // Check if rover has reached target position
-    if (distance < 0.7) {
+
+    double currentYaw = rover.currentYaw;
+
+    double angleError = initialBearing - currentYaw;
+
+     if (angleError > 180.0) {
+         angleError -= 360.0;
+    } 
+     else if (angleError < -180.0) {
+         angleError += 360.0;
+     }
+    std::cout << "Initial bearing: " << initialBearing << " degrees" << std::endl;
+    std::cout << "Yaw: " << currentYaw << " Angle error: " << angleError << std::endl;
+
+    geometry_msgs::Twist twistMsg;
+
+if ((distance <= 0.3) && (std::fabs(angleError) <= 0.1)) {
         std::cout << "Rover reached target position." << std::endl;
-        geometry_msgs::Twist twistMsg;
+        //geometry_msgs::Twist twistMsg;
         twistMsg.linear.x = 0.0;
         twistMsg.angular.z = 0.0;
         std::cout << twistMsg << std::endl;
- if (ros::ok()) {
-    std::cout << "work1" << std::endl;
         cmdVelPub.publish(twistMsg);
-        std::cout << "work2" << std::endl;
+        std::cout<<"Published" << std::endl;
+        std::cout << "Enter target latitude: ";
+        std::cin >> targetLatitude;
+        std::cout << "Enter target longitude: ";
+        std::cin >> targetLongitude; 
+        ros::spinOnce();
+        loop_rate.sleep();
+        currentLatitude = rover.currentLatitude;
+        currentLongitude = rover.currentLongitude;
+        initialBearing = rover.calculateBearing(currentLatitude, currentLongitude, targetLatitude, targetLongitude);
+        std::cout << currentLatitude << " " << currentLongitude << std::endl;
+        std::cout << "Initial bearing: " << initialBearing << " degrees" << std::endl;
+        //break; // Exit the loop 
+}
 
- } 
-        break; // Exit the loop
-    }
+else if ((distance > 0.3) && (std::fabs(angleError) <= 0.1)) {
+        //isYawAligned = true; // Yaw is aligned with initial bearing
+        twistMsg.angular.z = 0.0;
+        twistMsg.linear.x = 0.3;  // Stop angular movement
+        std::cout << twistMsg << std::endl; 
+        cmdVelPub.publish(twistMsg);} 
 
-    // Calculate angle error between initial bearing and current yaw
-    double currentYaw = rover.getCurrentYaw();
-
-    double angleError;
-
-    if ((currentLatitude >= 0 && currentLongitude >= 0) || (currentLatitude < 0 && currentLongitude < 0))  {
-        angleError = initialBearing - currentYaw; 
-        std::cout << "hi" << std::endl;}
-    else {
-        angleError = initialBearing - currentYaw + 180;
-        std::cout << "bye" << std::endl;
-    }
-
-    if (angleError > 180.0) {
-        angleError -= 360.0;
-    } else if (angleError < -180.0) {
-        angleError += 360.0;
-    }
-    
-    std::cout << "Yaw: " << currentYaw << " Angle error: " << angleError << std::endl;
-
-    // Adjust rover's orientation towards initial bearing while moving linearly
-    geometry_msgs::Twist twistMsg;
-
-    if (std::fabs(angleError) <= 0.1) {
-        isYawAligned = true; // Yaw is aligned with initial bearing
-        twistMsg.angular.z = 0.0; // Stop angular movement
-    } else {
-        isYawAligned = false; // Yaw is not aligned with initial bearing
+else {
+        // isYawAligned = false; // Yaw is not aligned with initial bearing
         twistMsg.angular.z = angleError * 0.2; // Angular velocity (rad/s)
+        twistMsg.linear.x = 0.0;
+        std::cout << twistMsg << std::endl;
+        cmdVelPub.publish(twistMsg);
     }
 
     
-    if (isYawAligned) {
-        twistMsg.linear.x = 0.3; // Linear velocity (m/s)
-        std::cout << twistMsg << std::endl;
-    } else {
-        twistMsg.linear.x = 0.0; // Stop linear movement until yaw is aligned
-        std::cout << twistMsg << std::endl;
-    }
-if (ros::ok()) {
-    std::cout << "works3" << std::endl;
-    cmdVelPub.publish(twistMsg);}
-
     ros::spinOnce();
     loop_rate.sleep();
 }
 
-*/
 
 
     return 0; 
